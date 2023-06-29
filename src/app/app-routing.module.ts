eceed1110e44066d82c3866ba8cb7a25797bdbc3
Osvaldo Luiz Novinski Junior <osvaldo.novinski@hotmail.com>
2023-06-29 17:34:57 -0300

    Alteração de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,20 @@ import { PokedexPageComponent } from './view/pokedex-page/pokedex-page.component
 import { PokedexPageModule } from './view/pokedex-page/pokedex-page.module';
 
 const routes: Routes = [
-  {
-    path:'pokedex', loadChildren:()=>import('./view/pokedex-page/pokedex-page.module').then(a=>a.PokedexPageModule),
-    component:PokedexPageComponent
-  }
+  
+    { path: '', redirectTo: '/pokedex', pathMatch: 'full' },
+    { path: 'pokedex', component: PokedexPageComponent },
+
+    
+    // path:'**',
+    // children:[
+    //   {
+    //     path:'pokedex', loadChildren:()=>import('./view/pokedex-page/pokedex-page.module').then(a=>a.PokedexPageModule),
+    //     component:PokedexPageComponent
+    //   }
+    // ]
+  
+  
 ];
 
 @NgModule({
